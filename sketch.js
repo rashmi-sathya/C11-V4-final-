90211bd418323dba7d058a7cbfb541b19b6dcd87
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-07-06 10:52:10 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,8 +3,6 @@ var ground, invisibleGround, groundImage;
 
 function preload(){
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
-  trex_collided = loadImage("trex_collided.png");
-  groundImage = loadImage("ground2.png")
 }
 
 function setup() {
